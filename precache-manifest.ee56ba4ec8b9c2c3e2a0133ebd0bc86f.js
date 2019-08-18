5225a5d0eb6b7a35b3a4028c14ea2249b369f6ee
IndimaDias <indimad@yahoo.com>
2019-08-18 08:18:50 -0700

    Updates

---
+++ precache-manifest.ee56ba4ec8b9c2c3e2a0133ebd0bc86f.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "d536b4b13827ad40f1bc",
+    "url": "/google-book-search/static/js/runtime~main.aaf871bd.js"
+  },
+  {
+    "revision": "69adb80c78dba3c74247",
+    "url": "/google-book-search/static/js/main.babda1dc.chunk.js"
+  },
+  {
+    "revision": "d2d15774686050d4e4e7",
+    "url": "/google-book-search/static/js/2.dbfec03c.chunk.js"
+  },
+  {
+    "revision": "69adb80c78dba3c74247",
+    "url": "/google-book-search/static/css/main.fd816b60.chunk.css"
+  },
+  {
+    "revision": "50e6b53de1c95139bf7a0957569a3e92",
+    "url": "/google-book-search/index.html"
+  }
+];
